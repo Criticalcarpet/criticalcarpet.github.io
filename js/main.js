913855fcb51188a2de2eb298195843d802254cf6
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-01-22 12:14:13 +0530

    ref : add shortcut 50%

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 console.log('I don\'t know why you are here');
+window.addEventListener("keyup", (event) => {
+        switch (event.code) {
+            case 'KeyG':
+                alert('Redirecting to the repo...\nClick OK to continue');
+                setTimeout(() => {
+                window.location.href = 'https://github.com/Criticalcarpet/criticalcarpet.github.io';
+                }, 1000);
+                break;
+            case 'KeyC':
+                alert('Redirecting to the github profile...\nClick OK to continue');
+                setTimeout(() => {
+                window.location.href = 'https://github.com/Criticalcarpet/';
+                }, 1000);
+        }
+});
