ed3a80dbf201aeb78fa17fc8b500018ae3269485
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-05-28 13:16:03 +0530

    test: workflow(deploy)

--- build.js
+++ build.js
@@ -5,15 +5,30 @@ child_process.execSync('npx vite build', { stdio: 'inherit' });
 
 console.info('[vite] Build completed successfully');
 
-fs.readdirSync('./dist/src').forEach(file => {
-    if (file.endsWith('.html')) {
-        const content = fs.readFileSync(`./dist/src/${file}`, 'utf8');
-        fs.writeFileSync(`./dist/${file}`, content, 'utf-8');
+fs.readdirSync('./').forEach(file => {
+    // delete all files except dist and build
+    if (file !== 'dist' && file !== 'build' && file !== 'node_modules' && file !== 'CNAME') {
+        if(fs.lstatSync(file).isDirectory()) {
+            fs.rmdirSync(file);
+        } 
+        fs.unlinkSync(file);
     }
-    });
+})
+// copy the contents of ./dist into the root including the folders and its content
+fs.copyFileSync('./dist/src/index.html', './index.html');
+fs.copyFileSync('./dist/src/404.html', './404.html');
+
+fs.readdirSync('./dist/assets').forEach(file => {
+    fs.copyFileSync('./dist/assets/' + file, './assets/' + file);
+})
+
+/*fs.readdirSync('./dist').forEach(file => {
+        const content = fs.readFileSync(`./dist/${file}`, 'utf8');
+        fs.writeFileSync(`./${file}`, content, 'utf-8');
+    });*/
 
 console.info('[build.js] Copied files to dist successfully');
 
-fs.rmSync('./dist/src', { recursive: true });
+// fs.rmSync('./dist/src', { recursive: true });
 
 console.info('[build.js] Removed src successfully');
